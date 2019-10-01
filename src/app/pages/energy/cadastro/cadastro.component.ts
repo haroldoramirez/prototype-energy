6ef76420e4a9f67d73eb6d3d714faad5471b8112
Haroldo Ramirez da Nóbrega <haroldo.nobrega@cibiogas.org>
2019-10-01 14:05:13 -0300

    estudando os componentes

--- src/app/pages/energy/cadastro/cadastro.component.ts
+++ src/app/pages/energy/cadastro/cadastro.component.ts
@@ -5,13 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
 })
+
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+  // tslint:disable-next-line: no-console
+    console.log('Componente Cadastro Energy criando...');
   }
 
+  ngOnInit() {
+      // tslint:disable-next-line: no-console
+      console.log('Componente Cadastro Energy já criado');
+  }
 
+  teste = 'teste';
+
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  trocarNome() {
+    this.teste = 'Teste trocado';
+  }
 
 }
